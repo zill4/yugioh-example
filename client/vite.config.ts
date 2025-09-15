431cc2a5215869dfb18120bd811e0e2442b378a0
Justin Crisp <justin@crispcode.io>
2025-09-14 21:35:40 -0500

    base project files for webspatial added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webSpatial from "@webspatial/vite-plugin";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    webSpatial(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          XR_ENV: process.env.XR_ENV,
+        },
+      },
+    }),
+  ],
+});
